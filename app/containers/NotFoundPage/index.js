5f3aa848554ee878d47a7715b9653588806f6904
D U C T R A N <ductm.official@gmail.com>
2021-09-19 17:27:35 +0700

    feat(notfound): update layout and style

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -5,15 +5,46 @@
  *
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { Helmet } from 'react-helmet';
+import { navDelay } from 'utils/constants';
+
+import Button from './Button';
+import Wrapper from './Wrapper';
+import Title from './Title';
+import Subtitle from './Subtitle';
 
 import messages from './messages';
 
 export default function NotFound() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), navDelay);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <div>
+      <Helmet title="Page Not Found" />
+
+      <TransitionGroup component={null}>
+        {isMounted && (
+          <CSSTransition timeout={500} classNames="fadeup">
+            <Wrapper className="fillHeight">
+              <Title>404</Title>
+              <Subtitle>
+                <FormattedMessage {...messages.header} />
+              </Subtitle>
+              <Button className="btn-big" to="/">
+                Go Home
+              </Button>
+            </Wrapper>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+    </div>
   );
 }
