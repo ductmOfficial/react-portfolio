6eefca1f8e92bbbaed58c11f8e59b185b7e87d30
D U C T R A N <ductm.official@gmail.com>
2021-09-19 19:04:38 +0700

    fix(app): insert missing constant

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -4,3 +4,16 @@ export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
 
 export const navDelay = 1000;
 export const loaderDelay = 2000;
+
+export const KEY_CODES = {
+  ARROW_LEFT: 'ArrowLeft',
+  ARROW_LEFT_IE11: 'Left',
+  ARROW_RIGHT: 'ArrowRight',
+  ARROW_RIGHT_IE11: 'Right',
+  ESCAPE: 'Escape',
+  ESCAPE_IE11: 'Esc',
+  TAB: 'Tab',
+  SPACE: ' ',
+  SPACE_IE11: 'Spacebar',
+  ENTER: 'Enter',
+};
